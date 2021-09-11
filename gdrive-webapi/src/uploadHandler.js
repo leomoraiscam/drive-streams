d15eff75f9ee737c052e905c082505d19c29609c
lmorais <leozonnn@gmail.com>
2021-09-11 17:12:11 -0300

    implementation on file funtion in uploadHandler

---
+++ gdrive-webapi/src/uploadHandler.js
@@ -0,0 +1,39 @@
+import Busboy from 'busboy';
+import { pipeline } from 'stream/promises'
+import { logger } from './logger';
+import fs from 'fs';
+
+
+export default class UploadHandler {
+  constructor({ io, socketId, downloadsFolder }) {
+    this.io = io,
+      this.socketId = socketId,
+      this.downloadsFolder = downloadsFolder
+  }
+
+  handleFileBytes() {
+
+  }
+
+  async onFile(fieldname, file, filename) {
+    const saveTo = `${this.downloadsFolder}/${filename}`
+
+    await pipeline(
+      file,
+      this.handleFileBytes.apply(this, [filename]),
+      fs.createWriteStream(saveTo)
+    )
+
+    logger.info(`File [${filename}] finished`)
+  }
+
+
+  registerEvents(headers, onFinish) {
+    const busboy = new Busboy({ headers });
+
+    busboy.on('file', this.onFile.bind(this));
+    busboy.on('finish', onFinish);
+
+    return busboy;
+  }
+}
