8c28b1a71da9b0363a008078199bca5008d5deb2
lmorais <leozonnn@gmail.com>
2021-09-29 23:37:25 -0300

    implementation freeze stress

--- gdrive-webapi/src/uploadHandler.js
+++ gdrive-webapi/src/uploadHandler.js
@@ -4,21 +4,31 @@ import { logger } from './logger';
 import fs from 'fs';
 
 export default class UploadHandler {
-  constructor({ io, socketId, downloadsFolder }) {
+  constructor({ io, socketId, downloadsFolder, messageTimeDelay = 200 }) {
     this.io = io;
     this.socketId = socketId;
     this.downloadsFolder = downloadsFolder;
     this.ON_UPLOAD_EVENT = 'file-upload';
+    this.messageTimeDelay = messageTimeDelay;
+  }
+
+  canExecute(lastExecution) {
+    return ( Date.now() -  lastExecution) > this.messageTimeDelay;
   }
 
   handleFileBytes(filename) {
     let processAlready = 0;
+    this.lastMessageSent = Date.now();
 
     async function* handleData(source) {
       for await (const chunk of source) {
         yield chunk;
 
         processAlready += chunk.length;
+
+        if(!this.canExecute(this.lastMessageSent)) {
+          continue
+        }
 
         this.io.to(this.socketId).emit(this.ON_UPLOAD_EVENT, {
           processAlready,
